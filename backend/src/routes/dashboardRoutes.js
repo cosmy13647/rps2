0c6faebdb4eec5b19e76105f0f6d45c97465d368
Cosmas Kibet <ckibet105@gmail.com>
2026-07-28 02:31:00 -0700

    fix

--- backend/src/routes/dashboardRoutes.js
+++ backend/src/routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
+const dashboardController = require('../controllers/DashboardController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
 /**
